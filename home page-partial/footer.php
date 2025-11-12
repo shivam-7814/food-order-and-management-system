fb63e060b91f32c9e22bf6c2061be61d5627b4e2
shivam-7814 <shivamsdoc7814@gmail.com>
2025-11-12 13:44:42 +0530

    Add files via upload

---
+++ home page-partial/footer.php
@@ -0,0 +1,157 @@
+ <div class="footer-details-container">
+
+   
+          <div class="footer-details">
+              <div class="top-footer">
+                    <div class="flex-box">
+                      <div class="footer-boxes">
+                        <h3>COMPANY</h3>
+                        <a href="">About Us</a>
+                        <a href="">Team</a>
+                        <a href="">Careers</a>
+                        <a href="">Blog</a>
+                       
+                          </div>
+                      <div  class="footer-boxes" >
+                            <h3>CONTACT</h3>
+                            <a href="">Help & Support</a>
+                            <a href="">Ride with us</a>
+                            <a href="">Carees</a>
+                           
+
+                          </div>
+                      <div  class="footer-boxes">
+                            <h3>LEGAL</h3>
+                            <a href="">Terms & Conditions</a>
+                            <a href="">Refund & Cancellation</a>
+                            <a href="">Privacy Policy</a>
+                            <a href="">Cookie Policy</a>
+                              <!-- <h2>
+                                Receive exclusive offers and
+                                discounts in your mailbox
+                              </h2> -->
+                              </div>
+                      <div class="footer-boxes follows-box">
+                          <h3>FOLLOW US</h3>
+                             <div class="flex-box social-links">
+                              <a href="">
+                              <svg fill="#c3c3c3" width="44px" height="44px" viewBox="-1.68 -1.68 27.36 27.36" xmlns="http://www.w3.org/2000/svg" stroke="#c3c3c3" stroke-width="0.6"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 2.03998C6.5 2.03998 2 6.52998 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.84998C10.44 7.33998 11.93 5.95998 14.22 5.95998C15.31 5.95998 16.45 6.14998 16.45 6.14998V8.61998H15.19C13.95 8.61998 13.56 9.38998 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96C15.9164 21.5878 18.0622 20.3855 19.6099 18.57C21.1576 16.7546 22.0054 14.4456 22 12.06C22 6.52998 17.5 2.03998 12 2.03998Z"></path> </g></svg>
+
+                              </a>
+                              <a href="">
+                              <svg fill="#c3c3c3" width="42px" height="42px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-143 145 512 512" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M113,145c-141.4,0-256,114.6-256,256s114.6,256,256,256s256-114.6,256-256S254.4,145,113,145z M272.8,560.7 c-20.8,20.8-44.9,37.1-71.8,48.4c-27.8,11.8-57.4,17.7-88,17.7c-30.5,0-60.1-6-88-17.7c-26.9-11.4-51.1-27.7-71.8-48.4 c-20.8-20.8-37.1-44.9-48.4-71.8C-107,461.1-113,431.5-113,401s6-60.1,17.7-88c11.4-26.9,27.7-51.1,48.4-71.8 c20.9-20.8,45-37.1,71.9-48.5C52.9,181,82.5,175,113,175s60.1,6,88,17.7c26.9,11.4,51.1,27.7,71.8,48.4 c20.8,20.8,37.1,44.9,48.4,71.8c11.8,27.8,17.7,57.4,17.7,88c0,30.5-6,60.1-17.7,88C309.8,515.8,293.5,540,272.8,560.7z"></path> <path d="M234.3,313.1c-10.2,6-21.4,10.4-33.4,12.8c-9.6-10.2-23.3-16.6-38.4-16.6c-29,0-52.6,23.6-52.6,52.6c0,4.1,0.4,8.1,1.4,12 c-43.7-2.2-82.5-23.1-108.4-55c-4.5,7.8-7.1,16.8-7.1,26.5c0,18.2,9.3,34.3,23.4,43.8c-8.6-0.3-16.7-2.7-23.8-6.6v0.6 c0,25.5,18.1,46.8,42.2,51.6c-4.4,1.2-9.1,1.9-13.9,1.9c-3.4,0-6.7-0.3-9.9-0.9c6.7,20.9,26.1,36.1,49.1,36.5 c-18,14.1-40.7,22.5-65.3,22.5c-4.2,0-8.4-0.2-12.6-0.7c23.3,14.9,50.9,23.6,80.6,23.6c96.8,0,149.7-80.2,149.7-149.7 c0-2.3,0-4.6-0.1-6.8c10.3-7.5,19.2-16.7,26.2-27.3c-9.4,4.2-19.6,7-30.2,8.3C222.1,335.7,230.4,325.4,234.3,313.1z"></path> </g> </g></svg>
+
+                              </a>
+                              <a href="">
+                                <svg width="44px" height="44px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#BDBDBD"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>instagram [#167]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-340.000000, -7439.000000)" fill="#BDBDBD"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M289.869652,7279.12273 C288.241769,7279.19618 286.830805,7279.5942 285.691486,7280.72871 C284.548187,7281.86918 284.155147,7283.28558 284.081514,7284.89653 C284.035742,7285.90201 283.768077,7293.49818 284.544207,7295.49028 C285.067597,7296.83422 286.098457,7297.86749 287.454694,7298.39256 C288.087538,7298.63872 288.809936,7298.80547 289.869652,7298.85411 C298.730467,7299.25511 302.015089,7299.03674 303.400182,7295.49028 C303.645956,7294.859 303.815113,7294.1374 303.86188,7293.08031 C304.26686,7284.19677 303.796207,7282.27117 302.251908,7280.72871 C301.027016,7279.50685 299.5862,7278.67508 289.869652,7279.12273 M289.951245,7297.06748 C288.981083,7297.0238 288.454707,7296.86201 288.103459,7296.72603 C287.219865,7296.3826 286.556174,7295.72155 286.214876,7294.84312 C285.623823,7293.32944 285.819846,7286.14023 285.872583,7284.97693 C285.924325,7283.83745 286.155174,7282.79624 286.959165,7281.99226 C287.954203,7280.99968 289.239792,7280.51332 297.993144,7280.90837 C299.135448,7280.95998 300.179243,7281.19026 300.985224,7281.99226 C301.980262,7282.98483 302.473801,7284.28014 302.071806,7292.99991 C302.028024,7293.96767 301.865833,7294.49274 301.729513,7294.84312 C300.829003,7297.15085 298.757333,7297.47145 289.951245,7297.06748 M298.089663,7283.68956 C298.089663,7284.34665 298.623998,7284.88065 299.283709,7284.88065 C299.943419,7284.88065 300.47875,7284.34665 300.47875,7283.68956 C300.47875,7283.03248 299.943419,7282.49847 299.283709,7282.49847 C298.623998,7282.49847 298.089663,7283.03248 298.089663,7283.68956 M288.862673,7288.98792 C288.862673,7291.80286 291.150266,7294.08479 293.972194,7294.08479 C296.794123,7294.08479 299.081716,7291.80286 299.081716,7288.98792 C299.081716,7286.17298 296.794123,7283.89205 293.972194,7283.89205 C291.150266,7283.89205 288.862673,7286.17298 288.862673,7288.98792 M290.655732,7288.98792 C290.655732,7287.16159 292.140329,7285.67967 293.972194,7285.67967 C295.80406,7285.67967 297.288657,7287.16159 297.288657,7288.98792 C297.288657,7290.81525 295.80406,7292.29716 293.972194,7292.29716 C292.140329,7292.29716 290.655732,7290.81525 290.655732,7288.98792" id="instagram-[#167]"> </path> </g> </g> </g> </g></svg>
+                              </a>
+                                </div>
+                                <h2>Receive exclusive offers and <br>discounts in your mailbox</h2>
+                            <div class="input-container" >
+                              <div class="mail-svg">
+                                <svg version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 512 512" xml:space="preserve" fill="rgb(189, 189, 189)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <style type="text/css">  .st0{fill:rgb(189, 189, 189);}  </style> <g> <path class="st0" d="M496.563,68.828H15.438C6.922,68.828,0,75.75,0,84.281v30.391l256,171.547l256-171.563V84.281 C512,75.75,505.078,68.828,496.563,68.828z"></path> <path class="st0" d="M0,178.016v203.391c0,34.125,27.641,61.766,61.781,61.766h388.438c34.141,0,61.781-27.641,61.781-61.766V178 L256,349.563L0,178.016z"></path> </g> </g></svg>
+                              </div>
+                              <form action="" class="flex-box" id="subscribe-form" method="POST">
+                                <input class="subscribe-input" placeholder="Enter Email" type="text">
+                                <button class="subscribe-btn">Subscribe</button>
+                              </form>
+                              </div>
+                      </div>
+
+                    </div>
+                
+              </div>
+              <div class="bottom-footer">
+
+                <p>All rights Reserved <span class="copy-logo" >&copy;</span> Bhojan <span  class="bhojanlogo-pc-bottom">
+                <svg height="28px"  width="28px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <circle style="fill:#FEE187;" cx="256" cy="256" r="256"></circle> <circle style="fill:#FEE187;" cx="256" cy="256" r="256"></circle> </g> <g> <path style="fill:#FFB30E;" d="M276.97,511.14c130.212-10.559,232.865-118.551,234.977-250.98L395.609,145.318l-52.336,33.671 l7.482,7.482l-87.126,52.012L88.319,278.919l23.961,49.916l-7.702,6.703L276.97,511.14z"></path> <polygon style="fill:#FFB30E;" points="315.763,135.327 302.545,114.352 294.357,85.333 240.674,89.347 315.763,162.047 "></polygon> </g> <path style="fill:#930000;" d="M322.801,175.84c-2.167,0-4.184-1.37-4.906-3.539L290.63,90.505h-46.697 c-2.857,0-5.172-2.315-5.172-5.172s2.315-5.172,5.172-5.172h50.424c2.226,0,4.203,1.424,4.906,3.537l28.444,85.333 c0.903,2.708-0.56,5.639-3.27,6.54C323.894,175.754,323.343,175.84,322.801,175.84z"></path> <path style="fill:#C92F00;" d="M286.689,341.333h80.843c3.986,0,7.34-2.979,7.811-6.937l21.83-183.377 c0.557-4.68-3.098-8.797-7.811-8.797H264.857c-4.713,0-8.37,4.117-7.811,8.797l21.83,183.377 C279.349,338.354,282.705,341.333,286.689,341.333z"></path> <path style="fill:#930000;" d="M262.465,196.525l16.413,137.871c0.471,3.956,3.827,6.937,7.811,6.937h80.844 c3.986,0,7.34-2.981,7.811-6.937l16.413-137.871H262.465z"></path> <path style="fill:#E09112;" d="M99.556,256c0-47.128,38.205-85.333,85.333-85.333s85.333,38.205,85.333,85.333H99.556z"></path> <path style="fill:#F17228;" d="M184.889,170.667c-0.433,0-0.86,0.026-1.293,0.033V256h86.626 C270.222,208.872,232.017,170.667,184.889,170.667z"></path> <path style="fill:#E09112;" d="M96.97,305.457c0,7.127,0.855,14.053,2.462,20.685c2.232,9.218,10.464,15.725,19.947,15.725h131.019 c9.483,0,17.715-6.508,19.947-15.725c1.607-6.632,2.462-13.559,2.462-20.685H96.97z"></path> <path style="fill:#F17228;" d="M183.596,305.457v36.411h66.801c9.483,0,17.715-6.508,19.947-15.725 c1.607-6.632,2.462-13.559,2.462-20.685L183.596,305.457L183.596,305.457z"></path> <path style="fill:#B54324;" d="M99.556,284.444h170.667c7.854,0,14.222-6.368,14.222-14.222l0,0c0-7.854-6.368-14.222-14.222-14.222 H99.556c-7.854,0-14.222,6.368-14.222,14.222l0,0C85.333,278.076,91.701,284.444,99.556,284.444z"></path> <path style="fill:#C92F00;" d="M270.222,256h-86.626v28.444h86.626c7.854,0,14.222-6.368,14.222-14.222 C284.444,262.368,278.076,256,270.222,256z"></path> <path style="fill:#D35933;" d="M99.556,312.889h170.667c7.854,0,14.222-6.368,14.222-14.222l0,0c0-7.854-6.368-14.222-14.222-14.222 H99.556c-7.854,0-14.222,6.368-14.222,14.222l0,0C85.333,306.521,91.701,312.889,99.556,312.889z"></path> <path style="fill:#B54324;" d="M270.222,284.444h-86.626v28.444h86.626c7.854,0,14.222-6.368,14.222-14.222 S278.076,284.444,270.222,284.444z"></path> </g></svg>
+
+                </span>  2025</p>
+                <div class="dev-name flex-box-center">
+                  <p>Made with </p> 
+                  <svg  width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z" fill="#FFB30E"></path> </g></svg>
+                    
+                    <p>by </p>
+                  <a href="https://www.linkedin.com/in/shivam7814/" target="_blank"> Shivam Maurya</a>
+                </div>
+                 
+              </div>
+          </div>
+      </div>
+</div>
+
+                   
+            <script>
+      document.addEventListener("DOMContentLoaded", () => {
+        const container = document.getElementById("cardContainer");
+        const btnLeft = document.getElementById("btnLeft");
+        const btnRight = document.getElementById("btnRight");
+
+        if (!container) {
+          console.error("cardContainer not found");
+          return;
+        }
+
+        // Calculate scroll step as card width + gap
+        function getCardStep() {
+          const firstCard = container.querySelector(".card");
+          if (!firstCard) return 220; // fallback
+          const style = getComputedStyle(firstCard);
+          const marginLeft = parseInt(style.marginLeft) || 0;
+          const marginRight = parseInt(style.marginRight) || 0;
+          return firstCard.offsetWidth + marginLeft + marginRight + 668; // +16 = gap
+        }
+
+        function updateButtons() {
+          btnLeft.disabled = container.scrollLeft <= 0;
+          btnRight.disabled =
+            Math.ceil(container.scrollLeft + container.clientWidth) >=
+            container.scrollWidth;
+        }
+
+        btnLeft.addEventListener("click", () => {
+          const step = getCardStep();
+          container.scrollBy({ left: -step, behavior: "smooth" });
+        });
+
+        btnRight.addEventListener("click", () => {
+          const step = getCardStep();
+          container.scrollBy({ left: step, behavior: "smooth" });
+        });
+
+        container.addEventListener("scroll", updateButtons);
+        window.addEventListener("resize", updateButtons);
+
+        // initial state
+        updateButtons();
+      });
+
+
+
+      
+    //  js for start loader
+            const loadingBox = document.querySelector(".loader-container");
+            const minTime = 2000; 
+            const transitionTime = 500; 
+            const pageReadyTime = Date.now();
+
+            function hideLoader() {
+                const elapsedTime = Date.now() - pageReadyTime;
+                const delay = elapsedTime < minTime ? minTime - elapsedTime : 0;
+
+                setTimeout(() => {
+                    loadingBox.classList.add("hidden");
+                    setTimeout(() => {
+                        loadingBox.remove();
+                    }, transitionTime); 
+                }, delay);
+            }
+
+            window.addEventListener("load", hideLoader());
+
+      
+    </script>
+   
+
+    </body>
+</html>
